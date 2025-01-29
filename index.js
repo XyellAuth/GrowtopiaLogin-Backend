cdfca3f9b583acab69355541f546be0bd5c1514e
Xyell User <128926830+XyellAuth@users.noreply.github.com>
2025-01-29 06:23:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -53,16 +53,13 @@ app.all('/player/growid/login/validate', (req, res) => {
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
-app.all('/player/growid/checktoken', (req, res) => {
-    const refreshToken = req.body;
-    let data = {
-        status: "success",
-        message: "Account Validated",
-        token = refreshToken,
-        url: "",
-        accountType: "growtopia"
-    };
-    res.send(data);
+app.all("/player/growid/checktoken", (req, res) => {
+  console.log(req.body);
+  const refreshToken = req.body.refreshToken;
+  console.log(req.body.refreshToken);
+    res.send(
+      `{"status":"success","message":"Account Validated.","token": "${refreshToken}","url":"","accountType":"growtopia"}`
+    );
 });
 app.get('/', function (req, res) {
     res.send('Hello World!');
